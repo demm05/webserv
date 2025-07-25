679a23e572a397bcbb8a16f516e4d44aafc06ac2
Dmytro Melnyk <dmelnyk@student.42london.com>
2025-07-25 18:31:13 +0100

    chore: Testing framework and makefile for it

---
+++ mk/tests.mk
@@ -0,0 +1,32 @@
+DOCTEST_DIR		=	lib/doctest/doctest
+TEST_NAME		=	run_tests
+TEST_DIR		=	tests
+TEST_SRCS		:=	$(shell find $(TEST_DIR) -name "*.cpp")
+TEST_OBJS		:=	$(patsubst $(TEST_DIR)/%.cpp,$(ODIR)/tests/%.o,$(TEST_SRCS))
+
+TEST_CXXFLAGS	=	$(filter-out -Werror, $(CXXFLAGS)) -I$(DOCTEST_DIR) -std=c++17
+
+PROJECT_OBJS_FOR_TESTS	:=	$(filter-out $(ODIR)/main.o, $(OBJS))
+
+DIRS	+=	$(ODIR)/tests
+
+t test: | $(DOCTEST_DIR)
+	@$(FMAKE) build_tests > /dev/null
+	@./$(TEST_NAME) -ni
+
+build_tests: $(TEST_NAME)
+
+$(TEST_NAME): $(TEST_OBJS) $(PROJECT_OBJS_FOR_TESTS)
+	@$(CXX) $(TEST_CXXFLAGS) -o $@ $^
+
+$(ODIR)/tests/%.o: $(TEST_DIR)/%.cpp | $(DIRS)
+	@echo "🔨 Compiling Test $<"
+	@$(CXX) $(TEST_CXXFLAGS) -c -o $@ $<
+
+test_clean:
+	@rm -rf $(TEST_NAME)
+
+$(DOCTEST_DIR):
+	@$(MAKE) init
+
+.PHONY: t test build_tests test_clean
