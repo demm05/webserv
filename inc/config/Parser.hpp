3c2a17910c9d52949fb6ac4663047277fd558648
Dmytro Melnyk <dmelnyk@student.42london.com>
2025-08-04 12:58:22 +0100

    feat(config): Now we've got a place for config data to be stored at

--- inc/config/Parser.hpp
+++ inc/config/Parser.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "Lexer.hpp"
+#include "ServerConfig.hpp"
+
 /* GRAMMAR in EBNF
  * 	config_file = { server_block } ;
  *	server_block = "server" "{" { statement } "}" ;
@@ -9,8 +12,12 @@
  *	parameter = IDENTIFIER | STRING | NUMBER ;
  */
 
-class ParseConfig {
+class Parser {
 public:
-    ParseConfig(int num_of_configs, char const **configs);
-    ~ParseConfig();
+    Parser(char const &fpath);
+    ~Parser();
+
+private:
+    ServerConfig config_;
+    Lexer lexer_;
 };
