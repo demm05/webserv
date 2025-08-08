823d842cbe8f1ad5969ded2f215b107e3869007d
Dmytro Melnyk <dmelnyk@student.42london.com>
2025-08-08 17:36:07 +0100

    chore: Token output stream for printing

--- inc/config/Token.hpp
+++ inc/config/Token.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <string>
-#include <iostream>
+#include <ostream>
 
 typedef enum TokenType {
     IDENTIFIER,
@@ -18,18 +18,9 @@ typedef enum TokenType {
 typedef struct Token {
     std::string literal;
     TokenType type;
-    bool isTypeIn(TokenType const arr[], size_t size) const {
-        if (!arr)
-            return false;
-        for (size_t i = 0; i < size; i++) {
-            if (arr[i] == type)
-                return true;
-        }
-        return false;
-    }
-    bool isAValue() {
-        static const TokenType validTypes[] = {IDENTIFIER, STRING, NUMBER};
-        static const size_t size = sizeof(validTypes) / sizeof(validTypes[0]);
-        return isTypeIn(validTypes, size);
-    }
+
+    bool isTypeIn(TokenType const arr[], size_t size) const;
 } Token;
+
+std::ostream &operator<<(std::ostream &o, Token const &t);
+std::ostream &operator<<(std::ostream &o, TokenType t);
