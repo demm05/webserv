0ba0835d0b5c59bc89e37fd78a796d7305aa21f8
shenghongzhong <howdavidlearncoding@gmail.com>
2025-08-12 18:06:15 +0100

    chore: commit epollmanager.hpp

---
+++ inc/EpollManager.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <stdint.h>
+#include <sys/epoll.h>
+
+class EpollManager {
+public:
+    EpollManager();
+    ~EpollManager();
+
+    void addFd(int fd, uint32_t events);
+    void removeFd(int fd);
+    void modifyFd(int fd, uint32_t events);
+
+    int waitForEvents(struct epoll_event *events, int max_events, int timeout = -1);
+
+private:
+    int _epollFd;
+
+    EpollManager(const EpollManager &other);
+    EpollManager &operator=(const EpollManager &other);
+};
